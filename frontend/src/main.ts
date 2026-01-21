4ee15e93e45329c49214810fca581869133326b8
fridacardonae-hash <fridacardonae@gmail.com>
2026-01-21 13:03:07 -0700

    Updated main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,24 @@
 import './style.css'
 
+const chat = document.getElementById("chat") as HTMLDivElement;
 const askBtn = document.getElementById("askBtn") as HTMLButtonElement;
 const questionInput = document.getElementById("question") as HTMLTextAreaElement;
-const answerBox = document.getElementById("answer") as HTMLPreElement;
+
+function addMessage(text: string, sender: "user" | "bot"){
+  const message = document.createElement("div");
+  message.classList.add("message", sender);
+  message.textContent = text;
+  chat.appendChild(message);
+  chat.scrollTop = chat.scrollHeight;
+}
 
 askBtn.addEventListener("click", async () => {
   const question = questionInput.value;
+  if (!question) return;
+  addMessage(question, "user");
+  questionInput.value = "";
 
-  answerBox.textContent = "Thinking...";
+  addMessage("Thinking...", "bot");
 
   try{
     const response = await fetch ("http://127.0.0.1:8080/ask", {
@@ -15,12 +26,14 @@ askBtn.addEventListener("click", async () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({question})
+      body: JSON.stringify({question}),
     });
-    const data = await response.json()
-    answerBox.textContent = data.answer;
+    const data = await response.json();
+
+    chat.lastChild?.remove();
+    addMessage(data.answer, "bot");
   } catch (err){
-    answerBox.textContent = "Error connecting to backend";
+    addMessage("Error connecting to AI backend", "bot");
     console.error(err)
   }
 });
